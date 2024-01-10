9dee6a8f9afa4a27b88fa2a5213f4f489d0e98ae
MurRtriX <155149209+MurRtriX@users.noreply.github.com>
2024-01-10 16:04:55 +0300

    Update install8.sh

--- install8.sh
+++ install8.sh
@@ -152,7 +152,7 @@ EOF
         apt -y install iptables-persistent
         iptables -t nat -A PREROUTING -i $(ip -4 route ls|grep default|grep -Po '(?<=dev )(\S+)'|head -1) -p udp --dport "$first_number":"$second_number" -j DNAT --to-destination :$remote_udp_port
         ip6tables -t nat -A PREROUTING -i $(ip -4 route ls|grep default|grep -Po '(?<=dev )(\S+)'|head -1) -p udp --dport "$first_number":"$second_number" -j DNAT --to-destination :$remote_udp_port
-        iptables -A INPUT -p tcp --dport $remote_udp_port -j ACCEPT
+        iptables -A INPUT -p udp --dport $remote_udp_port -j ACCEPT
         netfilter-persistent save
         sysctl net.ipv4.conf.all.rp_filter=0
         sysctl net.ipv4.conf.$(ip -4 route ls|grep default|grep -Po '(?<=dev )(\S+)'|head -1).rp_filter=0
