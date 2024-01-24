a67817eb59a6082d4f95a328b634d46220f1c7fa
MurRtriX <155149209+MurRtriX@users.noreply.github.com>
2024-01-24 15:38:13 +0300

    Update install8.sh

--- install8.sh
+++ install8.sh
@@ -11,11 +11,10 @@ fi
 cd /root
 clear
 echo -e "$YELLOW
-Hysteria Installer by Resleeved"
-echo "Version : 1"
+ 💚 RESLEEVED NET HYSTERIA SCRIPT 💚      "
 echo -e "$NC
 Select an option"
-echo "1. Install UDP Hysteria"
+echo "1. INSTALL UDP HYSTERIA"
 echo "2. Exit"
 selected_option=0
 
@@ -38,16 +37,20 @@ clear
 case $selected_option in
     1)
         echo -e "$YELLOW"
-        echo "Installing UDP Hysteria ..."
+        echo "     🧡 UDP HYSTERIA AUTO INSTALLATION 🧡      "
+        echo "          💚 Installing Binaries 💚           "
         echo -e "$NC"
-        apt-get update && apt-get upgrade
-        apt update && apt upgrade
+        apt-get update && apt-get upgrade 
+        apt update && apt upgrade 
         apt install net-tools
+        systemctl stop hysteria-server.service
+        rm -f /etc/systemd/system/hysteria-server.service
+        rm -rf /root/hy
         mkdir hy
         cd hy
         udp_script="/root/hy/hysteria-linux-amd64"
         if [ ! -e "$udp_script" ]; then
-            wget github.com/apernet/hysteria/releases/download/v1.3.5/hysteria-linux-amd64
+            wget https://github.com/JohnReaJR/A/releases/download/V1/hysteria-linux-amd64
         fi
         chmod 755 hysteria-linux-amd64
         openssl ecparam -genkey -name prime256v1 -out ca.key
@@ -97,7 +100,6 @@ case $selected_option in
         fi
         sudo debconf-set-selections <<< "iptables-persistent iptables-persistent/autosave_v4 boolean true"
         sudo debconf-set-selections <<< "iptables-persistent iptables-persistent/autosave_v6 boolean true"
-        apt -y install iptables-persistent
         while true; do
             echo -e "$YELLOW"
             read -p "Binding UDP Ports : from port : " first_number
@@ -122,9 +124,38 @@ case $selected_option in
                 echo -e "$NC"
             fi
         done
+        #Install Badvpn
+        cd /root
+        systemctl stop udpgw.service
+        rm -f /etc/systemd/system/udpgw.service
+        rm -f /usr/bin/udpgw
+        cd /usr/bin
+        wget https://github.com/JohnReaJR/A/releases/download/V1/udpgw
+        chmod 755 udpgw
+        cat <<EOF >/etc/systemd/system/udpgw.service
+[Unit]
+Description=UDPGW Gateway Service by InFiNitY 
+After=network.target
+
+[Service]
+Type=forking
+ExecStart=/usr/bin/screen -dmS udpgw /bin/udpgw --listen-addr 127.0.0.1:7300 --max-clients 1000 --max-connections-for-client 100
+Restart=always
+User=root
+
+[Install]
+WantedBy=multi-user.target
+EOF
+        #start badvpn
+        systemctl enable udpgw.service
+        systemctl start udpgw.service
+        echo -e "$YELLOW"
+        echo "     🧡 P2P SERVICE INITIALIZED 🧡     "
+        echo "        💚 Badvpn Activated 💚         "
+        echo -e "$NC"
+    
         # [+config+]
         chmod 755 /root/hy/config.json
-
         cat <<EOF >/etc/systemd/system/hysteria-server.service
 [Unit]
 After=network.target nss-lookup.target
@@ -144,24 +175,27 @@ LimitNOFILE=infinity
 WantedBy=multi-user.target
 EOF
         #Start Services
-        apt -y install iptables-persistent
         iptables -t nat -A PREROUTING -i $(ip -4 route ls|grep default|grep -Po '(?<=dev )(\S+)'|head -1) -p udp --dport "$first_number":"$second_number" -j DNAT --to-destination :$remote_udp_port
         ip6tables -t nat -A PREROUTING -i $(ip -4 route ls|grep default|grep -Po '(?<=dev )(\S+)'|head -1) -p udp --dport "$first_number":"$second_number" -j DNAT --to-destination :$remote_udp_port
+        iptables -A INPUT -p udp --dport $remote_udp_port -j ACCEPT
+        ip6tables -A INPUT -p udp --dport $remote_udp_port -j ACCEPT
         sysctl net.ipv4.conf.all.rp_filter=0
         sysctl net.ipv4.conf.$(ip -4 route ls|grep default|grep -Po '(?<=dev )(\S+)'|head -1).rp_filter=0
         echo "net.ipv4.ip_forward = 1
         net.ipv4.conf.all.rp_filter=0
         net.ipv4.conf.$(ip -4 route ls|grep default|grep -Po '(?<=dev )(\S+)'|head -1).rp_filter=0" > /etc/sysctl.conf
         sysctl -p
-        sudo iptables-save > /etc/iptables/rules.v4
-        sudo ip6tables-save > /etc/iptables/rules.v6
+        netfilter-persistent save
+        netfilter-persistent reload
+        netfilter-persistent start
         systemctl enable hysteria-server.service
         systemctl start hysteria-server.service
-        lsof -i :"$remote_udp_port"
-        echo "UDP Hysteria installed successfully, please check the logs above"
-        echo "IP Address :"
-        echo "Obfs : '"$obfs"'"
-        echo "auth str : '"$auth_str"'"
+        cd /root
+        rm .bash_history && history -c
+        echo -e "$YELLOW"
+        echo "    💚 UDP HYSTERIA INSTALLED SUCCESSFULLY 💚        "
+        echo "       🧡 Telegram >>> t.me/Am_The_Last_Envoy 🧡   "
+        echo -e "$NC"
         exit 1
         ;;
     2)
