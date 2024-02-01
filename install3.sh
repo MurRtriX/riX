a8068cced39da9d8d187c53cf87217d370e8eb0b
MurRtriX <155149209+MurRtriX@users.noreply.github.com>
2024-02-01 12:03:24 +0300

    Update install3.sh

--- install3.sh
+++ install3.sh
@@ -13,6 +13,40 @@ echo "        ╰┈➤💚 Installing DNSTT Binaries 💚          "
 echo -e "$NC"
 apt-get update && apt-get upgrade
 apt update && apt upgrade
+ufw disable
+apt-get remove --auto-remove ufw
+apt-get purge ufw
+apt-get purge --auto-remove ufw
+apt-get remove ufw
+iptables -P INPUT ACCEPT
+iptables -P FORWARD ACCEPT
+iptables -P OUTPUT ACCEPT
+iptables -F
+iptables -X 
+iptables -Z
+iptables -t nat -F
+iptables -t nat -X
+iptables -t mangle -F
+iptables -t mangle -X
+iptables -t raw -F
+iptables -t raw -X
+apt-get install iptables
+apt-get install iptables-persistent
+ip6tables -P INPUT ACCEPT
+ip6tables -P FORWARD ACCEPT
+ip6tables -P OUTPUT ACCEPT
+ip6tables -F
+ip6tables -X 
+ip6tables -Z
+ip6tables -t nat -F
+ip6tables -t nat -X
+ip6tables -t mangle -F
+ip6tables -t mangle -X
+ip6tables -t raw -F
+ip6tables -t raw -X
+netfilter-persistent save
+netfilter-persistent reload
+netfilter-persistent start
 apt install -y git golang-go
 git clone https://www.bamsoftware.com/git/dnstt.git
 cd dnstt/dnstt-server
