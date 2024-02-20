4a181165fdd88359c268a54bd7637a2c92231618
MurRtriX <155149209+MurRtriX@users.noreply.github.com>
2024-02-20 13:43:52 +0300

    Update iptables.sh

--- iptables.sh
+++ iptables.sh
@@ -60,6 +60,7 @@ sysctl -w net.core.rmem_max=16777216
 sysctl -w net.core.wmem_max=16777216
 sysctl -w net.ipv4.tcp_rmem=8192
 sysctl -w net.ipv4.tcp_wmem=8192
+sysctl -w vm.drop_caches=3
 sysctl -w vm.swappiness=10
 sysctl -w vm.dirty_ratio=60
 sysctl -w vm.dirty_background_ratio=2
