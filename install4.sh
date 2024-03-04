cbc4c1c49c717ff9749223149ed55486eb0d1656
MurRtriX <155149209+MurRtriX@users.noreply.github.com>
2024-03-04 12:49:08 +0300

    Update install4.sh

--- install4.sh
+++ install4.sh
@@ -58,7 +58,6 @@ sysctl -w net.ipv4.ip_forward=1
 echo 1 > /proc/sys/net/ipv4/ip_forward
 sysctl -p
 rm -rf /root/iodine-0.7.0
-apt-get install iodine
 wget http://code.kryo.se/iodine/iodine-0.7.0.tar.gz
 tar xzvf iodine-0.7.0.tar.gz
 cd iodine-0.7.0
