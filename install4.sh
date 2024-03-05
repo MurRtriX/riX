cf76ca516ff10054ced199a28ace5bc8e0a7c482
MurRtriX <155149209+MurRtriX@users.noreply.github.com>
2024-03-05 04:57:39 +0300

    Update install4.sh

--- install4.sh
+++ install4.sh
@@ -53,10 +53,11 @@ iptables -t filter -A FORWARD -i $(ip -4 route ls|grep default|grep -Po '(?<=dev
 iptables -t filter -A FORWARD -i dns0 -o $(ip -4 route ls|grep default|grep -Po '(?<=dev )(\S+)'|head -1) -j ACCEPT
 iptables -A FORWARD -i dns0 -o $(ip -4 route ls|grep default|grep -Po '(?<=dev )(\S+)'|head -1) -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT 
 iptables -A FORWARD -i $(ip -4 route ls|grep default|grep -Po '(?<=dev )(\S+)'|head -1) -o dns0 -m state --state ESTABLISHED,RELATED -j ACCEPT
-iptables -A FORWARD -i $(ip -4 route ls|grep default|grep -Po '(?<=dev )(\S+)'|head -1) -o dns0 -m state --state  NEW,RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -i $(ip -4 route ls|grep default|grep -Po '(?<=dev )(\S+)'|head -1) -o dns0 -m state --state NEW,RELATED,ESTABLISHED -j ACCEPT
 iptables -A FORWARD -i $(ip -4 route ls|grep default|grep -Po '(?<=dev )(\S+)'|head -1) -o dns0 -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -t nat -A POSTROUTING -o $(ip -4 route ls|grep default|grep -Po '(?<=dev )(\S+)'|head -1) -j MASQUERADE
-iptables -t nat -A POSTROUTING -j SNAT --to-source 139.84.236.25
+iptables --t nat -A PREROUTING --source 10.0.0.1 -p udp --destination-port 53 -j ACCEPT
+iptables --t nat -A PREROUTING --in-i $(ip -4 route ls|grep default|grep -Po '(?<=dev )(\S+)'|head -1) -p udp --destination-port 53 -j REDIRECT --to-ports 53
 netfilter-persistent save
 netfilter-persistent reload
 netfilter-persistent start
