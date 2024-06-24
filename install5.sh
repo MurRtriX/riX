4229d50214983431b1dfdd18fa2f6747d1218199
MurRtriX <155149209+MurRtriX@users.noreply.github.com>
2024-06-24 12:20:03 +0300

    Update install5.sh

--- install5.sh
+++ install5.sh
@@ -27,6 +27,15 @@
             fi
         done
         cd /root
+        iptables -t nat -A PREROUTING -p tcp --dport "$http_port" -j REDIRECT --to-port "$http_port"
+        iptables -A INPUT -p tcp --dport "$http_port" -j ACCEPT
+        netfilter-persistent save
+        netfilter-persistent reload
+        netfilter-persistent start
+        cd /root
+        sudo systemctl stop tcp-server.service
+        sudo systemctl disable tcp-server.service
+        rm -rf /etc/systemd/system/tcp-server.service
         rm -rf /usr/bin/tcp-linux-amd64
         cd /usr/bin
         http_script="/root/tcp/tcp-linux-amd64"
@@ -34,14 +43,26 @@
             wget http://github.com/JohnReaJR/A/releases/download/V1/tcp-linux-amd64
         fi
         chmod 755 tcp-linux-amd64
+        cd /root
         ##Tcp Auto Service
-        
-        screen -dmS tcp ./tcp-linux-amd64 -addr :"$http_port" dstAddr 127.0.0.1:22
-        iptables -t nat -A PREROUTING -p tcp --dport "$http_port" -j REDIRECT --to-port "$http_port"
-        iptables -A INPUT -p tcp --dport "$http_port" -j ACCEPT
-        netfilter-persistent save
-        netfilter-persistent reload
-        netfilter-persistent start
+        cat <<EOF >/etc/systemd/system/dnstt-server.service
+        [Unit]
+        Description=UDPGW Gateway Service by InFiNitY 
+        After=network.target
+
+        [Service]
+        Type=forking
+        ExecStart=/usr/bin/screen -dmS tcp /bin/tcp-linux-amd64 -addr :"$http_port" dstAddr 127.0.0.1:22
+        Restart=always
+        User=root
+
+        [Install]
+        WantedBy=multi-user.targe
+        EOF
+        ##Start Tcp service
+        systemctl daemon-reload
+        sudo systemctl enable tcp-server.service
+        sudo systemctl start tcp-server.service 
         echo -e "$YELLOW"
         echo "    💚 TCP INSTALLATION DONE💚   "
         echo "    ╰┈➤💚 TCP Running 💚       "
