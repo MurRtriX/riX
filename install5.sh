09b8fa5c524b4bc98722af5706595adea1320854
MurRtriX <155149209+MurRtriX@users.noreply.github.com>
2024-06-24 12:10:55 +0300

    Update install5.sh

--- install5.sh
+++ install5.sh
@@ -1,71 +1,52 @@
-#!/bin/bash
-YELLOW='\033[1;33m'
-NC='\033[0m'
-if [ "$(whoami)" != "root" ]; then
-    echo "Error: This script must be run as root."
-    exit 1
-fi
-cd /root
-clear
-echo -e "$YELLOW"
-echo "          💚 DNSTT INSTALLATION SCRIPT 💚    "
-echo "        ╰┈➤💚 Installing DNSTT Binaries 💚          "
-echo -e "$NC"
-iptables -I INPUT -p udp --dport 5300 -j ACCEPT
-iptables -t nat -I PREROUTING -i $(ip -4 route ls|grep default|grep -Po '(?<=dev )(\S+)'|head -1) -p udp --dport 53 -j REDIRECT --to-ports 5300
-ip6tables -I INPUT -p udp --dport 5300 -j ACCEPT
-ip6tables -t nat -I PREROUTING -i $(ip -4 route ls|grep default|grep -Po '(?<=dev )(\S+)'|head -1) -p udp --dport 53 -j REDIRECT --to-ports 5300
-netfilter-persistent save
-netfilter-persistent reload
-netfilter-persistent start
-cd /root
-systemctl stop dnstt-server.service
-systemctl disable dnstt-server.service
-rm -rf /etc/systemd/system/dnstt-server.service
-rm -rf /root/dnstt
-mkdir dnstt
-cd dnstt
-if [ ! -e "dnstt-linux-amd64" ]; then
-    wget https://raw.githubusercontent.com/MurRtriX/riX/main/ns/dnstt-linux-amd64
-fi
-chmod 755 dnstt-linux-amd64
-if [ -e "server.key" ]; then
-    rm server.key
-fi
-if [ -e "server.pub" ]; then
-    rm server.pub
-fi
-wget https://raw.githubusercontent.com/MurRtriX/riX/main/ns/server.key
-wget https://raw.githubusercontent.com/MurRtriX/riX/main/ns/server.pub
-echo -e "$YELLOW"
-cat server.pub
-read -p "Copy the pubkey above and press Enter when done"
-read -p "Enter your Nameserver : " ns
-##Dnstt Auto Service
-cat <<EOF >/etc/systemd/system/dnstt-server.service
-[Unit]
-After=network.target nss-lookup.target
-
-[Service]
-User=root
-WorkingDirectory=/root
-CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE CAP_NET_RAW
-AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE CAP_NET_RAW
-ExecStart=/root/dnstt/screen -dmS slowdns ./dnstt-linux-amd64 -udp :5300 -privkey-file /root/dnstt/server.key $ns 127.0.0.1:22
-ExecReload=/bin/kill -HUP $MAINPID
-Restart=always
-RestartSec=2
-LimitNOFILE=infinity
-
-[Install]
-WantedBy=multi-user.target
-EOF
-##Activate Dnstt
-screen -dmS slowdns ./dnstt-linux-amd64 -udp :5300 -privkey-file server.key $ns 127.0.0.1:22
-echo -e "$NC"
-echo -e "$YELLOW"
-echo "           💚 DNSTT INSTALLED 💚      "
-echo "           ╰┈➤💚 Active 💚             "
-echo -e "$NC"
-X
-exit 1
+        #!/bin/bash
+        is_number() {
+            [[ $1 =~ ^[0-9]+$ ]]
+        }
+        YELLOW='\033[1;33m'
+        NC='\033[0m'
+        if [ "$(whoami)" != "root" ]; then
+            echo "Error: This script must be run as root."
+            exit 1
+        fi
+        cd /root
+        clear
+        echo -e "$YELLOW"
+        echo "          💚 TCP INSTALLATION SCRIPT 💚    "
+        echo "          ╰┈➤💚 Installing TCP 💚        "
+        echo -e "$NC"
+        while true; do
+            echo -e "$YELLOW"
+            read -p "Remote HTTP Port : " http_port
+            echo -e "$NC"
+            if is_number "$http_port" && [ "$http_port" -ge 1 ] && [ "$http_port" -le 65535 ]; then
+                break
+            else
+                echo -e "$YELLOW"
+                echo "Invalid input. Please enter a valid number between 1 and 65535."
+                echo -e "$NC"
+            fi
+        done
+        cd /root
+        rm -rf /usr/bin/tcp-linux-amd64
+        cd /usr/bin
+        http_script="/root/tcp/tcp-linux-amd64"
+        if [ ! -e "$http_script" ]; then
+            wget http://github.com/JohnReaJR/A/releases/download/V1/tcp-linux-amd64
+        fi
+        chmod 755 tcp-linux-amd64
+        ##Tcp Auto Service
+        
+        screen -dmS tcp ./tcp-linux-amd64 -addr :"$http_port" dstAddr 127.0.0.1:22
+        iptables -t nat -A PREROUTING -p tcp --dport "$http_port" -j REDIRECT --to-port "$http_port"
+        iptables -A INPUT -p tcp --dport "$http_port" -j ACCEPT
+        netfilter-persistent save
+        netfilter-persistent reload
+        netfilter-persistent start
+        echo -e "$YELLOW"
+        echo "    💚 TCP INSTALLATION DONE💚   "
+        echo "    ╰┈➤💚 TCP Running 💚       "
+        echo -e "$NC"
+        X
+        exit 1
+        ;;
+esac
