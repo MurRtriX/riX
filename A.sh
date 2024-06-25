b6bf4539f74cead03934f06599ffb445cb7a4921
MurRtriX <155149209+MurRtriX@users.noreply.github.com>
2024-06-25 15:42:25 +0300

    Update A.sh

--- A.sh
+++ A.sh
@@ -20,8 +20,7 @@ clear && clear
 figlet -k ResleevedNet | lolcat
 echo -e "\033[1;34m   ResleevedNet v.5 \033[0m  | \033[1;33m v.5 Release  | ResleevedNet \033[0m"
 echo -e "\033[1;36m╰═════════════════════════════════════════════════════╯\033[0m"
-echo ""
-sudo apt-get update && sudo apt-get upgrade -y; echo ""; echo""; echo""; echo""; echo""; echo""; echo""; clear
+echo""; echo""; echo""; sudo apt-get update && sudo apt-get upgrade -y; echo ""; echo""; echo""; echo""; echo""; echo""; echo""; echo""; echo""; clear
 rm -rf /etc/V; cd /etc; mkdir V; cd V; mkdir bin; mkdir auth; mkdir -p /etc/V/auth/passwds; cd /root; cd /etc/V/bin; clear
 wget "https://raw.githubusercontent.com/MurRtriX/riX/main/V/atom.sh" -O atom.sh; clear && clear && chmod 755 atom.sh; clear && clear
 echo ""
