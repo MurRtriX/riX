0f404a4c4d1d40c66085fee13c83e7c7c554be08
MurRtriX <155149209+MurRtriX@users.noreply.github.com>
2024-06-25 16:32:06 +0300

    Update A.sh

--- A.sh
+++ A.sh
@@ -17,7 +17,7 @@ printf "\033[1;33m ╰┈➤ 💚 Resleeved Net Ultimate Installer 💚 \033[1;3
 }
 update_packages() {
 clear && clear
-sudo apt-get update && sudo apt-get upgrade -y; echo""; echo""; echo""; echo""; echo""; echo""; echo""; echo""; echo""; echo""; clear
+sudo apt-get update && sudo apt-get upgrade -y; clear
 rm -rf /etc/V; cd /etc; mkdir V; cd V; mkdir bin; mkdir auth; mkdir -p /etc/V/auth/passwds; cd /root; cd /etc/V/bin; clear
 wget "https://raw.githubusercontent.com/MurRtriX/riX/main/V/atom.sh" -O atom.sh; clear && clear && chmod 755 atom.sh; clear && clear
 echo ""
