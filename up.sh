22aa11c374e20eb71d33ee0d43f60ebd80837f89
MurRtriX <155149209+MurRtriX@users.noreply.github.com>
2024-07-01 14:08:09 +0300

    Update up.sh

--- up.sh
+++ up.sh
@@ -11,43 +11,6 @@ echo -e "$YELLOW"
 echo "          💚 IPTABLES....SETTING UP YOUR FIREWALL 💚    "
 echo "             ╰┈➤💚 Resleeved Net Firewall 💚          "
 echo -e "$NC"
-apt install wget
-apt install net-tools
-ufw disable
-apt-get remove --purge ufw firewalld -y
-apt-get remove ufw
-apt-get remove --auto-remove ufw
-apt-get purge ufw
-apt-get purge --auto-remove ufw
-iptables -P INPUT ACCEPT
-iptables -P FORWARD ACCEPT
-iptables -P OUTPUT ACCEPT
-iptables -F
-iptables -X 
-iptables -Z
-iptables -t nat -F
-iptables -t nat -X
-iptables -t mangle -F
-iptables -t mangle -X
-iptables -t raw -F
-iptables -t raw -X
-apt-get install iptables
-apt-get install iptables-persistent
-ip6tables -P INPUT ACCEPT
-ip6tables -P FORWARD ACCEPT
-ip6tables -P OUTPUT ACCEPT
-ip6tables -F
-ip6tables -X 
-ip6tables -Z
-ip6tables -t nat -F
-ip6tables -t nat -X
-ip6tables -t mangle -F
-ip6tables -t mangle -X
-ip6tables -t raw -F
-ip6tables -t raw -X
-netfilter-persistent save
-netfilter-persistent reload
-netfilter-persistent start
 rm -f /etc/sysctl.conf
 sysctl net.ipv4.conf.all.rp_filter=0
 sysctl net.ipv4.conf.$(ip -4 route ls|grep default|grep -Po '(?<=dev )(\S+)'|head -1).rp_filter=0
@@ -55,17 +18,17 @@ echo "net.ipv4.ip_forward=1
 net.ipv4.conf.all.rp_filter=0
 net.ipv4.conf.$(ip -4 route ls|grep default|grep -Po '(?<=dev )(\S+)'|head -1).rp_filter=0" >> /etc/sysctl.conf
 sysctl -p
-sysctl -w net.core.rmem_default=83886080
-sysctl -w net.core.wmem_default=83886080
+sysctl -w net.core.rmem_default=212992
+sysctl -w net.core.wmem_default=212992
 sysctl -w net.core.rmem_max=16777216
 sysctl -w net.core.wmem_max=16777216
-sysctl -w net.ipv4.tcp_rmem=8192
-sysctl -w net.ipv4.tcp_wmem=8192
-echo "net.core.rmem_default=83886080" >> /etc/sysctl.conf
-echo "net.core.wmem_default=83886080" >> /etc/sysctl.conf
+echo "net.core.rmem_default=212992" >> /etc/sysctl.conf
+echo "net.core.wmem_default=212992" >> /etc/sysctl.conf
 echo "net.core.rmem_max=16777216" >> /etc/sysctl.conf
 echo "net.core.wmem_max=16777216" >> /etc/sysctl.conf
-echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_keepalive_time=7200" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_keepalive_probes=9" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_keepalive_intvl=75" >> /etc/sysctl.conf
 sysctl -p /etc/sysctl.conf
 echo -e "$YELLOW"
 echo "           💚 FIREWALL CONFIGURED 💚      "
