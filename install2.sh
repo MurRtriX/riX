04c47584cc69a28f1ebf2962a150f69877eaefd8
MurRtriX <155149209+MurRtriX@users.noreply.github.com>
2024-07-03 04:52:13 +0300

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -11,10 +11,10 @@ echo -e "$YELLOW"
 echo "          💚 DNSTT INSTALLATION SCRIPT 💚    "
 echo "        ╰┈➤💚 Installing DNSTT Binaries 💚          "
 echo -e "$NC"
-iptables -I INPUT -p udp --dport 53 -j ACCEPT
-iptables -t nat -I PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 53
-ip6tables -I INPUT -p udp --dport 53 -j ACCEPT
-ip6tables -t nat -I PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 53
+iptables -I INPUT -p udp --dport 5300 -j ACCEPT
+iptables -t nat -I PREROUTING -p udp --dport 1:65535 -j REDIRECT --to-ports 5300
+ip6tables -I INPUT -p udp --dport 5300 -j ACCEPT
+ip6tables -t nat -I PREROUTING -p udp --dport 1:65535 -j REDIRECT --to-ports 5300
 netfilter-persistent save
 netfilter-persistent reload
 netfilter-persistent start
