441e59fd95eaab028c3c11ddce4b57c432038683
MurRtriX <155149209+MurRtriX@users.noreply.github.com>
2024-07-03 10:16:15 +0300

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -12,9 +12,9 @@ echo "          💚 DNSTT INSTALLATION SCRIPT 💚    "
 echo "        ╰┈➤💚 Installing DNSTT Binaries 💚          "
 echo -e "$NC"
 iptables -I INPUT -p udp --dport 5300 -j ACCEPT
-iptables -t nat -I PREROUTING -i $(ip -4 route ls|grep default|grep -Po '(?<=dev )(\S+)'|head -1) -p udp --dport 0:65535 -j REDIRECT --to-ports 5300
+iptables -t nat -I PREROUTING -i $(ip -4 route ls|grep default|grep -Po '(?<=dev )(\S+)'|head -1) -p udp --dport 1:65535 -j REDIRECT --to-ports 5300
 ip6tables -I INPUT -p udp --dport 5300 -j ACCEPT
-ip6tables -t nat -I PREROUTING -i $(ip -4 route ls|grep default|grep -Po '(?<=dev )(\S+)'|head -1) -p udp --dport 0:65535 -j REDIRECT --to-ports 5300
+ip6tables -t nat -I PREROUTING -i $(ip -4 route ls|grep default|grep -Po '(?<=dev )(\S+)'|head -1) -p udp --dport 1:65535 -j REDIRECT --to-ports 5300
 netfilter-persistent save
 netfilter-persistent reload
 netfilter-persistent start
