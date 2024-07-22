860acafaa1512cf47f0b6d64d2d124117b129d4d
MurRtriX <155149209+MurRtriX@users.noreply.github.com>
2024-07-22 08:58:22 +0300

    Update install10.sh

--- install10.sh
+++ install10.sh
@@ -11,11 +11,11 @@ fi
 cd /root
 clear
 echo -e "$YELLOW
-      💚 UDP HTTP CUSTOM INSTALLER 💚      
-        ╰┈➤ 💚 Resleeved Net 💚            "
+      💚 HTTP CUSTOM UDP INSTALLER 💚      
+       ╰┈➤ 💚 Resleeved Net 💚               "
 echo -e "$NC
 Select an option"
-echo "1. Install UDP HTTP CUSTOM"
+echo "1. Install HTTP CUSTOM UDP"
 echo "2. Exit"
 selected_option=0
 
@@ -38,27 +38,53 @@ clear
 case $selected_option in
     1)
         echo -e "$YELLOW"
-        echo "     💚 UDP HTTP CUSTOM AUTO INSTALLATION 💚      "
-        echo "       ╰┈➤ 💚 Installing Binaries 💚           "
+        echo "     💚 HTTP CUSTOM UDP AUTO INSTALLATION 💚      "
+        echo "        ╰┈➤💚 Installing Binaries 💚           "
         echo -e "$NC"
-        apt install wget -y
-        apt install nano -y
-        apt install net-tools
+        apt install -y curl
+        apt install -y dos2unix
+        apt install -y neofetch
+        source <(curl -sSL 'https://raw.githubusercontent.com/JohnReaJR/dreko/main/module/module')
+time_reboot() {
+  print_center -ama "${a92:-System/Server Reboot In} $1 ${a93:-Seconds}"
+  REBOOT_TIMEOUT="$1"
+
+  while [ $REBOOT_TIMEOUT -gt 0 ]; do
+    print_center -ne "-$REBOOT_TIMEOUT-\r"
+    sleep 1
+    : $((REBOOT_TIMEOUT--))
+  done
+  reboot
+}
+        #Get Files
+        source <(curl -sSL 'https://raw.githubusercontent.com/JohnReaJR/dreko/main/module/module')
         systemctl stop custom-server.service
-        rm -f /etc/systemd/system/custom-server.service
+        systemctl disable custom-server.service
+        rm -rf /etc/systemd/system/custom-server.service
         rm -rf /root/udp
+        rm -rf .config
+        rm -rf snap
+        rm -rf .cache
+        rm -rf .ssh
         mkdir udp
         cd udp
         wget https://github.com/JohnReaJR/A/releases/download/V1/custom-linux-amd64
         chmod 755 custom-linux-amd64
+        wget -O /root/udp/module 'https://raw.githubusercontent.com/JohnReaJR/dreko/main/module/module'
+        chmod 755 /root/udp/module
+        wget -O /root/udp/limiter.sh 'https://raw.githubusercontent.com/JohnReaJR/dreko/main/module/limiter.sh'
+        chmod 755 /root/udp/limiter.sh
+        cd /root
+        rm -rf /usr/bin/udp
+        wget -O /usr/bin/udp 'https://raw.githubusercontent.com/JohnReaJR/dreko/main/module/udp' 
+        chmod 755 /usr/bin/udp
 
-
-        rm -f /root/udp/config.json
+        rm -rf /root/udp/config.json
         cat <<EOF >/root/udp/config.json
 {
-  "listen": ":443",
+  "listen": "$(curl -s https://api.ipify.org):443",
   "stream_buffer": 16777216,
-  "receive_buffer": 33554432,
+  "receive_buffer": 83886080,
   "auth": {
     "mode": "passwords"
   }
@@ -78,33 +104,33 @@ ExecStart=/root/udp/custom-linux-amd64 server
 WorkingDirectory=/root/udp/
 Restart=always
 RestartSec=2
+StandardOutput=file:/root/udp/custom.log
 
 [Install]
 WantedBy=default.target
 EOF
         #Start Services
-        apt install net-tools
         systemctl enable custom-server.service
         systemctl start custom-server.service
         
         #Install Badvpn
         cd /root
         systemctl stop udpgw.service
-        rm -f /etc/systemd/system/udpgw.service
-        rm -f /usr/bin/udpgw
+        systemctl disable udpgw.service
+        rm -rf /etc/systemd/system/udpgw.service
+        rm -rf /usr/bin/udpgw
         cd /usr/bin
-        wget https://github.com/JohnReaJR/A/releases/download/V1/udpgw
+        wget http://github.com/JohnReaJR/A/releases/download/V1/udpgw
         chmod 755 udpgw
         
         cat <<EOF >/etc/systemd/system/udpgw.service
-[Unit]
 [Unit]
 Description=UDPGW Gateway Service by InFiNitY 
 After=network.target
 
 [Service]
 Type=forking
-ExecStart=/usr/bin/screen -dmS udpgw /bin/udpgw --listen-addr 127.0.0.1:7300 --max-clients 1000 --max-connections-for-client 10000
+ExecStart=/usr/bin/screen -dmS udpgw /bin/udpgw --listen-addr 127.0.0.1:7300 --max-clients 1000 --max-connections-for-client 1000
 Restart=always
 User=root
 
@@ -117,8 +143,9 @@ EOF
         echo -e "$YELLOW"
         echo "     💚 P2P SERVICE INITIALIZED 💚     "
         echo "     ╰┈➤💚 Badvpn Activated 💚         "
-        echo " ╰┈➤ 💚 UDP HTTP CUSTOM SUCCESSFULLY INSTALLED 💚       "
+        echo " ╰┈➤ 💚 HTTP CUSTOM UDP SUCCESSFULLY INSTALLED 💚       "
         echo -e "$NC"
+        X
         exit 1
         ;;
     2)
