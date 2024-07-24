e76bc51cc7d0ba95b58f310a063d8f2203775586
MurRtriX <155149209+MurRtriX@users.noreply.github.com>
2024-07-24 08:56:28 +0300

    Create aws.sh

---
+++ ns/aws.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+YELLOW='\033[1;33m'
+NC='\033[0m'
+if [ "$(whoami)" != "root" ]; then
+    echo "Error: This script must be run as root."
+    exit 1
+fi
+cd /root
+clear
+sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/; s/KbdInteractiveAuthentication no/KbdInteractiveAuthentication yes/; s/#PasswordAuthentication yes/PasswordAuthentication yes/' /etc/ssh/sshd_config; sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config.d/60-cloudimg-settings.conf; sudo service sshd restart; sudo sh -c "echo /bin/false >> /etc/shells" chsh -s /bin/false; sudo hostnamectl set-hostname Resleeved; sudo sh -c "echo 127.0.1.1 Resleeved >> /etc/hosts" chsh -s 127.0.1.1 Resleeved; sudo passwd root; exec bash; sudo rm .bash_history && history -c && history -w
+X
+exit 1
