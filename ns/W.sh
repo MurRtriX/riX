03e80eb224ad317c88055e1b1fdedea0654b0eea
MurRtriX <155149209+MurRtriX@users.noreply.github.com>
2024-07-25 10:31:36 +0300

    Update W.sh

--- ns/W.sh
+++ ns/W.sh
@@ -28,8 +28,8 @@ echo -e "$YELLOW Warp Services "$NC
  echo -e "\033[32m 0.  Exit \033[0m"
  selected_option=0
 
-while [ $selected_option -lt 1 ] || [ $selected_option -gt 5 ]; do
-    echo -e "\033[1;33m Select a number from 1 to 6: \033[0m"
+while [ $selected_option -lt 1 ] || [ $selected_option -gt 0 ]; do
+    echo -e "\033[1;33m Select a number from 1 to 5: \033[0m"
     read -p " " input
     
     # Check if input is a number
