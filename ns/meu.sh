4163d76d8001cfd42b736812c51caaaeed55de94
MurRtriX <155149209+MurRtriX@users.noreply.github.com>
2024-07-29 13:45:52 +0300

    Update meu.sh

--- ns/meu.sh
+++ ns/meu.sh
@@ -524,7 +524,7 @@ check_dependencies() {
 
 # 获取 warp 账户信息
 warp_api(){
-  local WARP_API_URL="https://raw.githubusercontent.com/MurRtriX/riX/main/ns/index.html"
+  local WARP_API_URL="raw.githubusercontent.com/MurRtriX/riX/main/ns/index.html"
   local RUN=$1
   local FILE_PATH=$2
   local WARP_LICENSE=$3
