d1f60b8307914b1eced3dae2d7ac1e51cd378318
MurRtriX <155149209+MurRtriX@users.noreply.github.com>
2024-08-03 13:39:26 +0300

    Update T.sh

--- T.sh
+++ T.sh
@@ -26,6 +26,29 @@ change_obfs(){
     green "The configuration is modified successfully, please re-import the client configuration file"
 }
 
+change_obfs(){
+    old_obfs=$(cat)
+    echo -e "ZIVPN UDP Passwords"
+read -p "Enter passwords separated by commas, example: pass1,pass2 (Press enter for Default 'zi'): " input_config
+
+    if [ -n "$input_config" ]; then
+        IFS=',' read -r -a config <<< "$input_config"
+        if [ ${#config[@]} -eq 1 ]; then
+            config+=(${config[0]})
+        fi
+    else
+        config=("zi")
+    fi
+
+    new_config_str="\"config\": [$(printf "\"%s\"," "${config[@]}" | sed 's/,$//')]"
+
+    sed -i -E "s/\"config\": ?\[[[:space:]]*\"zi\"[[:space:]]*\]/${new_config_str}/g" /etc/zivpn/config.json
+
+    systemctl restart hysteria-server.service
+
+    green "The configuration is modified successfully, please re-import the client configuration file"
+}
+
 menu() {
     clear
     echo ""
