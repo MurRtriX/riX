af2be8e582028e49d5cd7ecba213f37aa554b953
MurRtriX <155149209+MurRtriX@users.noreply.github.com>
2024-08-03 11:07:24 +0300

    Update T.sh

--- T.sh
+++ T.sh
@@ -16,11 +16,11 @@ inst_obfs(){
 change_obfs(){
     old_pwd=$(cat /root/hy/config.json | grep password | sed -n 2p | awk -F " " '{print $2}' | sed "s/\"//g" | sed "s/,//g")
     inst_obfs
-    sed -i "s/\"obfs\": \"\"/\"obfs\": \"$obfs_pwd\"/" /root/hy/config.json
+    sed -i 's/\"obfs\":\"\"/\"obfs\":\"$obfs_pwd\"/' /root/hy/config.json
 
-    sed -i "s/\"obfs\": \"$old_pwd\"/\"obfs\": \"$obfs_pwd\"/" /root/hy/config.json
+    sed -i 's/\"obfs\":\"$old_pwd\"/\"obfs\":\"$obfs_pwd\"/' /root/hy/config.json
 
-    sed -i "s/$old_pwd/$auth_pwd/" /root/hy/config.json
+    sed -i 's/$old_pwd/$auth_pwd/' /root/hy/config.json
 
     systemctl restart hysteria-server.service
 
