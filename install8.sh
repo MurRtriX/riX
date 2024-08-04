d3270e3f767b8d5a0508dff95685ab8bbb908c65
MurRtriX <155149209+MurRtriX@users.noreply.github.com>
2024-08-04 15:17:02 +0300

    Update install8.sh

--- install8.sh
+++ install8.sh
@@ -20,7 +20,7 @@ echo "2. OTHER UDP HYSTERIA SETTINGS"
 echo "3. Exit"
 # Select an Option
 
-    read -p "$(echo -e "\033[1;33m Select a number from 0 to 2: \033[0m")" input
+    read -p "$(echo -e "\033[1;33mSelect a number from 0 to 2: \033[0m")" input
     
     # Check if input is a number
     if [[ $input =~ ^[0-9]+$ ]]; then
