d1138f7b55c391c20bb93d5934cbc278edcb28b1
MurRtriX <155149209+MurRtriX@users.noreply.github.com>
2024-08-04 15:33:41 +0300

    Update k.sh

--- ns/k.sh
+++ ns/k.sh
@@ -90,6 +90,7 @@ case $selected_option in
     3)
         echo ""
         echo -e "\033[1;32mActive Auth/Users:\033[0m"
+        echo -e "\033[32m─────────────────•\033[0m"
         echo ""
         echo -e "\033[1;33m\033[1;36m[ \033[1;33m$(awk -F, 'NR==1 { print }' /root/hy/authusers | sed "s/\"/  /g" | sed "s/,/  /g") \033[1;36m]\033[0m"
         echo ""
