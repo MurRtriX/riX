26d776ddae220ce4738d1d97d0ccf46d77cad619
MurRtriX <155149209+MurRtriX@users.noreply.github.com>
2024-08-04 13:04:50 +0300

    Update install8.sh

--- install8.sh
+++ install8.sh
@@ -21,7 +21,7 @@ echo "3. Exit"
 selected_option=0
 
 while [ $selected_option -lt 1 ] || [ $selected_option -gt 3 ]; do
-    read -p "$(echo -e "\033[1;33mSelect a number from 1 to 3: \033[0m")" input
+    read -p "$(echo -e "\033[1;33m Select a number from 1 to 3: \033[0m")" input
     
     # Check if input is a number
     if [[ $input =~ ^[0-9]+$ ]]; then
