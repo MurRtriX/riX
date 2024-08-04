242a0cc0736945d63d50ad7b49f32ae678e66d3a
MurRtriX <155149209+MurRtriX@users.noreply.github.com>
2024-08-04 12:29:58 +0300

    Update k.sh

--- ns/k.sh
+++ ns/k.sh
@@ -90,10 +90,10 @@ case $selected_option in
         ;;
     3)
         echo ""
-        echo -e "\033[1;32mActive Auth/Passwords\033[0m"
-        echo -e "\033[1;36m───────────────────────•\033[0m"
-        echo -e "\033[1;33m$(awk -F, 'NR==1 { print }' /root/hy/authusers | sed "s/\"//g" | sed "s/,/                                                                         /g")\033[0m"
-        echo -e "\033[1;36m───────────────────────•\033[0m"
+        echo -e "\033[1;33mActive Auth/Passwords\033[0m"
+        echo ""
+        echo -e "\033[1;32m$(awk -F, 'NR==1 { print }' /root/hy/authusers | sed "s/\"//g" | sed "s/,/                                                                         /g")\033[0m"
+        echo -e "\033[1;33m───────────────────────•\033[0m"
         read -p "Press any key to exit ↩︎" key
         exit 1
         ;;
