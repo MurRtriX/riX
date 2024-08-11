2e62359fdde051c6d505a9ab699f491911e8a8b0
MurRtriX <155149209+MurRtriX@users.noreply.github.com>
2024-08-11 13:40:00 +0300

    Update k.sh

--- ns/k.sh
+++ ns/k.sh
@@ -24,7 +24,7 @@ echo -e "$YELLOW Hysteria UDP Services "$NC
  echo -e "\033[1;32m 3.  Active Users  \033[1;0m"
  echo -e "\033[1;32m 0.  Exit \033[0m"
  #Select an Option
-while [ $selected_option -gt 0 ] || [ $selected_option -gt 3  ]; do
+while [ $selected_option -gt 0 ] || [ $selected_option -lt 0  ]; do
     read -p "$(echo -e "\033[1;33m Select a number from 0 to 3: \033[0m")" input
     
     # Check if input is a number
