96462915999986d8a4ce2b22dff49a605eecbbcb
MurRtriX <155149209+MurRtriX@users.noreply.github.com>
2024-09-03 17:32:08 +0300

    Update m.sh

--- o/warp/m.sh
+++ o/warp/m.sh
@@ -524,7 +524,7 @@ check_dependencies() {
 
 # 获取 warp 账户信息
 warp_api(){
-  local WARP_API_URL="raw.githubusercontent.com/MurRtriX/riX/main/o/warp/k.json"
+  local WARP_API_URL="api.github.com/repos/MurRtriX/riX/releases/assets/190126868"
   local RUN=$1
   local FILE_PATH=$2
   local WARP_LICENSE=$3
