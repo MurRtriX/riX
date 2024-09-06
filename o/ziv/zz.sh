41d2915d02fb32dd34a91ca7074e48350b00835c
MurRtriX <155149209+MurRtriX@users.noreply.github.com>
2024-09-06 18:58:55 +0300

    Update zz.sh

--- o/ziv/zz.sh
+++ o/ziv/zz.sh
@@ -187,8 +187,7 @@ EOF
         echo "    💚 UDP HYSTERIA INSTALLED SUCCESSFULLY 💚        "
         echo "      ╰┈➤💚 Telegram >>> t.me/Am_The_Last_Envoy    "
         echo -e "$NC"
-        X
-        exit 1
+        exit 0
         ;;
     2)
             echo ""
@@ -225,8 +224,7 @@ EOF
         chmod 755 /root/zv/config.json
         systemctl restart ziv-server.service
         sleep 1
-        X
-        exit 1
+        exit 0
         ;;
     3)
         echo ""
@@ -235,12 +233,11 @@ EOF
         echo -e "\033[1;33m\033[1;36m[ \033[1;33m$(awk -F, 'NR==1 { print }' /root/zv/authusers | sed "s/\"/  /g" | sed "s/,/  /g") \033[1;36m]\033[0m"
         echo ""
         read -p "Press any key to exit ↩︎" key
-        X
-        exit 1
+        exit 0
         ;;
     *)
-        clear; X
-        exit 1
+        clear; 
+        exit 0
         ;;
 esac
 done
