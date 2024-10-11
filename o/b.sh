3c7f260418ad13027fba95ef7f61c542d1745868
MurRtriX <155149209+MurRtriX@users.noreply.github.com>
2024-10-11 14:50:10 +0300

    Create b.sh

---
+++ o/b.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+YELLOW='\033[1;33m'
+NC='\033[0m'
+if [ "$(whoami)" != "root" ]; then
+    echo "Error: This script must be run as root."
+    exit 1
+fi
+cd /root
+clear
+rm -rf /etc/issue.net
+
+exit 0
