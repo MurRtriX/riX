1120b846f67cd24fb4b838a531c8bff4cfe1f053
MurRtriX <155149209+MurRtriX@users.noreply.github.com>
2024-11-26 16:58:44 +0300

    Update wire.sh

--- o/wire.sh
+++ o/wire.sh
@@ -349,7 +349,7 @@ EOF
 		{ crontab -l 2>/dev/null; echo "$(( $RANDOM % 60 )) $(( $RANDOM % 3 + 3 )) * * * /usr/local/sbin/boringtun-upgrade &>/dev/null" ; } | crontab -
 	fi
 	echo
-        mkdir /root/etc/Wire
+        mkdir /etc/Wire
 	qrencode -t ANSI256UTF8 < /root/etc/Wire/"$client.conf"
 	echo -e '\xE2\x86\x91 That is a QR code containing the client configuration.'
 else
